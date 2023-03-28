b59d761a5751adb48202fe6b84dad90a59e07bd8
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-28 11:08:21 +0500

    Adds tab bar controller

---
+++ Workout/Screens/TabBarController.swift
@@ -0,0 +1,81 @@
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    private func setupTabBar() {
+        tabBar.tintColor = Resources.Colors.active
+        tabBar.barTintColor = Resources.Colors.active
+        tabBar.backgroundColor = .white
+        
+        tabBar.layer.borderColor = Resources.Colors.separator.cgColor
+        tabBar.layer.borderWidth = 1
+        tabBar.layer.masksToBounds = true
+        
+        let dataSource: [Tabs] = [.overview, .session, .progress, .settings]
+        
+        viewControllers = dataSource.map{
+            switch $0 {
+            case .overview:
+                let viewController = OverviewViewController()
+                return embedInNavigationController(viewController: viewController)
+            case .session:
+                let viewController = SessionViewController()
+                return embedInNavigationController(viewController: viewController)
+            case .progress:
+                let viewController = ProgressViewController()
+                return embedInNavigationController(viewController: viewController)
+            case .settings:
+                let viewController = SettingsViewController()
+                return embedInNavigationController(viewController: viewController)
+            }
+        }
+        
+        viewControllers?.enumerated().forEach {
+            $1.tabBarItem.title = dataSource[$0].title
+            $1.tabBarItem.image = UIImage(named: dataSource[$0].iconName)
+            $1.tabBarItem.tag = $0
+        }
+    }
+    
+    private func embedInNavigationController(viewController: UIViewController) -> UINavigationController {
+        UINavigationController(rootViewController: viewController)
+    }
+}
+
+enum Tabs: Int, CaseIterable {
+    case overview
+    case session
+    case progress
+    case settings
+    
+    var title: String {
+        switch self {
+        case .overview:
+            return "Overview"
+        case .session:
+            return "Session"
+        case .progress:
+            return "Progress"
+        case .settings:
+            return "Settings"
+        }
+    }
+    
+    var iconName: String {
+        switch self {
+        case .overview:
+            return "overview_tab"
+        case .session:
+            return "session_tab"
+        case .progress:
+            return "progress_tab"
+        case .settings:
+            return "settings_tab"
+        }
+    }
+}
