3477f38488801a2204be184273f54586d95e2e19
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-28 13:47:44 +0500

    Configures UINavigationBarAppearance

--- Workout/Screens/Navigations/NavBarController.swift
+++ Workout/Screens/Navigations/NavBarController.swift
@@ -9,13 +9,27 @@ final class NavBarController: UINavigationController {
     
     private func setupNavBar() {
         view.backgroundColor = .white
-        navigationBar.isTranslucent = false
-        navigationBar.standardAppearance.titleTextAttributes = [
+        
+        let standard = UINavigationBarAppearance()
+        standard.configureWithDefaultBackground()
+        standard.backgroundColor = .white
+        standard.titleTextAttributes = [
             .foregroundColor: UIColor.customTitleDarkGray,
             .font: UIFont.robotoMedium(with: 17)
         ]
+          
+        let button = UIBarButtonItemAppearance(style: .plain)
+        button.normal.titleTextAttributes = [.foregroundColor: UIColor.customActive]
+        button.disabled.titleTextAttributes = [.foregroundColor: UIColor.customInactive]
+        standard.buttonAppearance = button
+          
+        UINavigationBar.appearance().standardAppearance = standard
         
         navigationBar.addBottomBorder(with: .customSeparator, height: 1)
+    }
+    
+    func addNavBarButton() {
+        
     }
     
 }
