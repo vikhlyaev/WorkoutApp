392fa153390aedc1bba5d1de1d125887f5a777a7
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-28 11:07:05 +0500

    Adds tabbar icons

---
+++ Workout/Resources/Resources.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+enum Resources {
+    enum Colors {
+        static var active = UIColor(hexString: "#437BFE")
+        static var inactive = UIColor(hexString: "#929DA5")
+        static var separator = UIColor(hexString: "#E8ECEF")
+    }
+    
+    enum Strings {
+        enum Images {
+            
+        }
+    }
+}
