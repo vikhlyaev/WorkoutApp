e0ea3c3802c41c037f102bec06f1aff886da064d
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-28 11:07:53 +0500

    Adds empty view controllers

---
+++ Workout/Screens/Progress/ProgressViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class ProgressViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
---
+++ Workout/Screens/Session/SessionViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class SessionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
