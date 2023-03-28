021921ce34f7c0824ee11530a53b428c0eff94e8
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-28 12:03:07 +0500

    Moves uicolor in extension

--- Workout/Extensions/UIKit/UIColor + Extensions.swift
+++ Workout/Extensions/UIKit/UIColor + Extensions.swift
@@ -1,20 +1,23 @@
 import UIKit
 
 extension UIColor {
+    static var customActive = UIColor(hexString: "#437BFE")
+    static var customInactive = UIColor(hexString: "#929DA5")
+    static var customSeparator = UIColor(hexString: "#E8ECEF")
+    static var customTitleDarkGray = UIColor(hexString: "#545C77")
+    static var customBackground = UIColor(hexString: "#F8F9F9")
+    
+    
     convenience init(hexString: String) {
         let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int = UInt64()
         Scanner(string: hex).scanHexInt64(&int)
         let a, r, g, b: UInt64
         switch hex.count {
-        case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (a, r, g, b) = (255, 0, 0, 0)
+        case 3: (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default: (a, r, g, b) = (255, 0, 0, 0)
         }
         self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
     }
