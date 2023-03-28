a55f128bf7538750219d05438a5ee295a5c24d85
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-28 12:11:32 +0500

    Adds UIView extensions

---
+++ Workout/Extensions/UIKit/UIView + Extensions.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension UIView {
+    func addBottomBorder(with color: UIColor, height: CGFloat) {
+        let separator = UIView()
+        separator.backgroundColor = color
+        separator.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        separator.frame = CGRect(x: 0,
+                                 y: frame.height - height,
+                                 width: frame.width,
+                                 height: height)
+        addSubview(separator)
+    }
+}
