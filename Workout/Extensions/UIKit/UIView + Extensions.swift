61bdfd7c33f161946d45fbed27307a46318e776b
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-29 11:02:42 +0500

    Adds UIView extension

--- Workout/Extensions/UIKit/UIView + Extensions.swift
+++ Workout/Extensions/UIKit/UIView + Extensions.swift
@@ -11,4 +11,29 @@ extension UIView {
                                  height: height)
         addSubview(separator)
     }
+    
+    func makeSystem(_ button: UIButton) {
+        button.addTarget(self, action: #selector(handleIn), for: [
+            .touchDown,
+            .touchDragInside
+        ])
+        
+        button.addTarget(self, action: #selector(handleOut), for: [
+            .touchDragOutside,
+            .touchUpInside,
+            .touchUpOutside,
+            .touchDragExit,
+            .touchCancel
+        ])
+    }
+    
+    @objc
+    private func handleIn() {
+        UIView.animate(withDuration: 0.15) { self.alpha = 0.55 }
+    }
+    
+    @objc
+    private func handleOut() {
+        UIView.animate(withDuration: 0.15) { self.alpha = 1 }
+    }
 }
